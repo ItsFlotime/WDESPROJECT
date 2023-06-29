b6db303ef2d8a1244dec607157f23d296562ae85
ItsFlotime <127414525+ItsFlotime@users.noreply.github.com>
2023-06-29 08:07:13 -0500

    Delete website directory

--- website/contact.php
+++
@@ -1,5 +0,0 @@
-<?php
-
-$name = $_POST["Namein"];
-$email = $_POST["Emailin"];
-$message = $_POST["Messagein"];
--- website/js/consultation.js
+++
@@ -1,7 +0,0 @@
-function Submit (){
-    const consult = document.getElementById("consult").value;
-    
-    localstorage.setItem("SUBJECT", consult);
-    
-    return;
-}
